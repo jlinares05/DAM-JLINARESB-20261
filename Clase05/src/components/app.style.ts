fe49009d6ea8c0d023880e5cd4c7a502ec8eb165
jlinares05 <jlinaresb@unbosque.edu.co>
2026-03-12 02:06:41 -0500

    Update app.style.ts

--- Clase05/src/components/app.style.ts
+++ Clase05/src/components/app.style.ts
@@ -1,11 +1,16 @@
-import { StyleSheet } from "react-native"; 
-import { colors } from "./themes";
+import { StyleSheet } from "react-native";
 
-export const appStyles = StyleSheet. create({
-  safeArea: {
-  flex: 1, 
-  alignItems: 'center',
-  justifyContent: 'center',
-  backgroundColor: colors. white
+export const appStyles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white"
+  },
+
+  row: {
+    flexDirection: "row"
   }
-}); 
+
+});
